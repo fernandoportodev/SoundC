c9f12ed11f2cf62406f03e98a051286f44a7aace
Fernando Porto <fernandogomes143@gmail.com>
2025-02-01 01:18:42 -0300

    Implementado o Try-catch para tratativa do request na API em caso de erro exibir o Exception

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
 ﻿using (HttpClient client = new HttpClient())
 {
-    string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-    Console.WriteLine(response);
+    try
+    {
+        string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.jso");
+        Console.WriteLine(response);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+
+    }
 }
